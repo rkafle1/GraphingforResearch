589c2293397d9f566ef66e8b2688c6a1bb1c5e37
rkafle1 <rkafle@ucsd.edu>
2023-02-09 13:14:59 -0800

    Began Trying to graph multiple bars

--- graph.py
+++ graph.py
@@ -23,3 +23,29 @@ def GenerateBarGraphUniversity(y):
 
 for i in range(1, 6):
     GenerateBarGraphUniversity(i)
+
+def GenerateBarGraphCompare(y1, y2):
+    xdata = []
+    y1data = []
+    y2data = []
+    with open('UCTransferData.csv','r') as csvfile:
+        lines = csv.reader(csvfile, delimiter=',')
+        for row in lines:
+            xdata.append(row[0])
+            y1data.append(row[y1])
+            y2data.append(row[y1])
+    
+    for i in range(1, len(y1data)):
+        y1data[i] = float(y1data[i])
+        y2data[i] = float(y2data[i])
+    plt.bar(xdata[1:], y1data[1:], width = 0.5,label = "Universitiesvs." + y1data[0] +
+     "vs"+ y2data[0])
+    plt.bar(xdata[1:], y2data[1:], width = 0.5,label = "Universitiesvs." + y1data[0] +
+     "vs"+ y2data[0])
+    plt.xlabel('Universities')
+    plt.ylabel(y1data[0])
+    plt.title("Universitiesvs." + y1data[0] + "vs"+ y2data[0])
+    plt.legend()
+    plt.savefig("Universitiesvs." + y1data[0] +"vs"+ y2data[0]+ ".png")
+    plt.close()
+GenerateBarGraphCompare(1,2)
