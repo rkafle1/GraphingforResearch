15663f6e3411ab3f71621b3a70512dd796fa8be8
rkafle1 <rkafle@ucsd.edu>
2023-02-09 13:00:15 -0800

    Generated Graphs

--- graph.py
+++ graph.py
@@ -5,18 +5,21 @@ import pandas as pd
 def GenerateBarGraphUniversity(y): 
     xdata = []
     ydata = []
-    
     with open('UCTransferData.csv','r') as csvfile:
         lines = csv.reader(csvfile, delimiter=',')
         for row in lines:
             xdata.append(row[0])
             ydata.append(row[y])
+    
     for i in range(1, len(ydata)):
-        ydata[i] = int(ydata[i])
+        ydata[i] = float(ydata[i])
     plt.bar(xdata[1:], ydata[1:], width = 0.72,label = "Universitiesvs." + ydata[0])
     plt.xlabel('Universities')
     plt.ylabel(ydata[0])
     plt.title("Universitiesvs." + ydata[0])
     plt.legend()
-    plt.show()
-GenerateBarGraphUniversity(1)
+    plt.savefig("Universitiesvs." + ydata[0] + ".png")
+    plt.close()
+
+for i in range(1, 6):
+    GenerateBarGraphUniversity(i)
